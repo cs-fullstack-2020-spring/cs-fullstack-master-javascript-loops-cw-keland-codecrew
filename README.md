056093d45237ff862ba1b25199168e2ede845b46
autumn-ragland <raglanda998@gmail.com>
2020-02-20 18:33:55 -0600

    graded

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 # JavaScript Loops Classwork
-
+#### Check out my comments in your JS file
+#### Score : 2/5
 ## For Loops
 
-### Exercise 1:
+### Exercise 1: CORRECT
 - Use a for loop to print numbers -3 through 8
 
-### Exercise 2:
+### Exercise 2: INCORRECT
 - Use a for loop to count from -5 to 5 three times in a row
 
-### Exercise 3:
+### Exercise 3: -
 - Use a for loop to print only even numbers from -50 to 50
 
-### Exercise 4:
+### Exercise 4: -
 Start with the following array:
 `let high_scores = [2112, 1024, 9000, 9001, 7200, 2600, 10000]`
 
@@ -30,7 +31,7 @@ HIGH SCORES
 10000 OVER 9000!
 ```
 
-### Exercise 5:
+### Exercise 5: -
 - Prompt the user to enter a number 
 - Prompt the user to enter another number 
 - Create a for loop that counts from the first number to the second number they entered
@@ -38,14 +39,14 @@ HIGH SCORES
 
 ## While/Do While Loops
 
-### Exercise A:
+### Exercise A: PARTIAL
 - Print -10 through 5 using a counter and while loop.
 
-<!-- ### Exercise B:
+### Exercise B: INCOMPLETE
 - Write a While loop that counts backwards from 10 to 0
-- Log the current count at the end of each iteration EXCEPT if the current count is ```5``` -->
+- Log the current count at the end of each iteration EXCEPT if the current count is ```5```
 
-### Exercise C:
+### Exercise C: - 
 - Create an empty `name_array`
 - Ask the user ```Enter a word. Enter 'q' to quit.``` Add each word they enter to the array
 - Create a while loop that will keep asking for words to add to the array until they enter 'q'
--- clw.js
+++ clw.js
@@ -1,68 +1,44 @@
-// ### Exercise 1:
-// - Use a for loop to print numbers -3 through 8
-
-// for (let i = -3; i <= 8; i ++)
-
-// {
+// // ### Exercise 1:
+// // - Use a for loop to print numbers -3 through 8
+// for (let i = -3; i <= 8; i++) {
 //     console.log(i);
 // }
 
-
-
-
-// ### Exercise A:
-// - Print -10 through 5 using a counter and while loop.
-
+// // ### Exercise A:
+// // - Print -10 through 5 using a counter and while loop.
 // let i = -10;
-
-
-// while (i != 5)
-
-// {
-//     if (i++)
-//     {
+// while (i != 5) {
+//     if (i++) { // !! : you condition will always be true
 //         console.log(i);
-//     }     
-      
+//     }
+//     // !! : you should increment after you print to include 10
+
 // }
 
-
-
-
-// ### Exercise 2:
-// - Use a for loop to count from -5 to 5 three times in a row
-
-// for (let i = -5; i <= 5;i++)
-// {
-//     console.log (i)
+// // ### Exercise 2:
+// // - Use a for loop to count from -5 to 5 three times in a row
+// // !! : should have used a nested loop - check out the answer gist for an example
+// for (let i = -5; i <= 5; i++) {
+//     console.log(i)
 // }
-// for (let i = -5; i <= 5;i++)
-// {
-//     console.log (i)
+// for (let i = -5; i <= 5; i++) {
+//     console.log(i)
 // }
-// for (let i = -5; i <= 5;i++)
-// {
-//     console.log (i)
+// for (let i = -5; i <= 5; i++) {
+//     console.log(i)
 // }
 
-// ### Exercise B:
-// - Write a While loop that counts backwards from 10 to 0
-// - Log the current count at the end of each iteration EXCEPT 
-// if the current count is ```5``` -->
+// // ### Exercise B:
+// // - Write a While loop that counts backwards from 10 to 0
+// // - Log the current count at the end of each iteration EXCEPT 
+// // if the current count is ```5```
+// let i = 10;
+// while (i != 5) {
+//     if (i--) { // !! : your condition will always be true
+//         console.log(i);
+//     }
+//     // else (i = 5)
+//     // {
 
-
-let i = 10;
-
-
-while (i != 5)
-
-{
-    if (i--)
-    {
-        console.log(i);
-    } 
-    else (i = 5)
-    {
-        
-    }  
-}
+//     // }  
+// }
